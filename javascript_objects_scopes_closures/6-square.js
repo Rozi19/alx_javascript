aa29c5d94e1c7aebb1dba3b318f0c26424f4319e
Roza Alene <rozaalene@gmail.com>
2023-09-07 01:03:49 +0300

    remove this

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,9 +5,9 @@ class Square extends square{
     charPrint(c){
 
         if (c === undefined){
-            for (let j = 0; j < this.size; j++)
+            for (let j = 0; j < size; j++)
             {
-                for(let i = 0; i < this.size; i++) {
+                for(let i = 0; i < size; i++) {
                     process.stdout.write("X" + "");
                 }
                 console.log()
@@ -15,9 +15,9 @@ class Square extends square{
         }
 
         else {
-            for (let j = 0; j < this.size; j++)
+            for (let j = 0; j < size; j++)
             {
-                for(let i = 0; i < this.size; i++) {
+                for(let i = 0; i < size; i++) {
                     process.stdout.write(c + "");
                 }
                 console.log()
