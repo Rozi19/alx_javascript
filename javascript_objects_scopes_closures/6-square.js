9836889d839063d755b4191ba318b165b1ad4e21
Roza Alene <rozaalene@gmail.com>
2023-09-07 01:54:47 +0300

    if c undefined call supoer class method

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,29 +1,18 @@
 const square = require('./5-square')
 
 class Square extends square{
-
+    
+    
     charPrint(c){
 
+
         if (c === undefined){
-            for (let j = 0; j < size; j++)
-            {
-                for(let i = 0; i < size; i++) {
-                    process.stdout.write("X" + "");
-                }
-                console.log()
-            }
+
+            super.print()
         }
 
-        else {
-            for (let j = 0; j < size; j++)
-            {
-                for(let i = 0; i < size; i++) {
-                    process.stdout.write(c + "");
-                }
-                console.log()
-            }
-        }
     }
 }
 
 module.exports = Square
+
