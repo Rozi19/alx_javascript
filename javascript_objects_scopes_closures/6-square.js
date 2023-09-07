1e99c68c5184bf320ccdc4d663b7e173a444c7dc
Roza Alene <rozaalene@gmail.com>
2023-09-07 17:53:27 +0300

    for characters

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -10,7 +10,15 @@ class Square extends square{
 
             super.print()
         }
-
+        else {
+        for (let j = 0; j < this.height; j++)
+        {
+            for(let i = 0; i < this.width; i++) {
+                process.stdout.write(c + "");
+            }
+            console.log()
+        }
+    }
     }
 }
 
