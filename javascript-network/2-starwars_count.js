e19ab09d0aad5044481bb3e2beb56a3f93403a0b
Roza Alene <rozaalene@gmail.com>
2023-09-12 16:41:10 +0300

    to all https not only starwaralx

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const API_URL = process.argv[2];
+let movieCount = 0;
 
-const requ = require("request");
-const url = process.argv[2]
-requ.get(url, function (error, response, body) {
-    
-    const object = JSON.parse(body)
-    const filter = object.results.filter(films => {
-        return films.characters.includes("https://swapi-api.alx-tools.com/api/people/18/")
-    })
-    console.log(filter.length);
+request(API_URL, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const results = JSON.parse(body).results;
+  for (const filmIdx in results) {
+    const filmChars = results[filmIdx].characters;
+    for (const charIndex in filmChars) {
+      if (filmChars[charIndex].includes('18')) {
+        movieCount += 1;
+      }
+    }
+  }
+  console.log(movieCount);
 });
