975d1d0050dba0bad1edd759412b4df45ecfc841
Roza Alene <rozaalene@gmail.com>
2023-09-12 03:05:26 +0300

    rename

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+
 const requ = require('request')
 
 const url = process.argv[2]
 
 requ.get(url, { encoding: 'utf-8'})
-    .on('responce', responce => {
-        console.log("code: ", responce.statuscode)
-    })
+.on('response', response => {
+    console.log("Code: ", response.statusCode)
+}
+);
