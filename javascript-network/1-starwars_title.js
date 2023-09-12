b39668c4a0326ba1d3b585fa0c7b815589c3902a
Roza Alene <rozaalene@gmail.com>
2023-09-12 03:31:28 +0300

    changing the url

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -2,7 +2,7 @@
 
 const requ = require("request");
 const id = process.argv[2]
-const url = `https://swapi.dev/api/films/${id}`;
+const url = `https://swapi-api.alx-tools.com/api/${id}`;
 requ.get(url, { encoding: 'utf-8'})
 .on('data', data => {
     const responce = JSON.parse(data);
