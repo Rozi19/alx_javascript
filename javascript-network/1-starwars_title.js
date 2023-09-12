df468af51181d1745b07341132eda2221b191a8e
Roza Alene <rozaalene@gmail.com>
2023-09-12 04:23:27 +0300

    JSON.parse (<anonymous> try to fix this error

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -2,9 +2,10 @@
 
 const requ = require("request");
 const id = process.argv[2]
-const url = `https://swapi-api.alx-tools.com/api/${id}`;
-requ.get(url, { encoding: 'utf-8'})
-.on('data', data => {
-    const responce = JSON.parse(data);
-    console.log(responce.title);
-})
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+requ.get(url, function (error, response, body) {
+    
+    const object = JSON.parse(body)
+        console.log(object.title)
+});
