c0adb2696ebb13d02ecdc3ca3f523a52e0391860
Roza Alene <rozaalene@gmail.com>
2023-09-29 21:55:35 +0300

    change qurey by id and alert by getElementById

--- html_js_validation/3-form-submission.js
+++ html_js_validation/3-form-submission.js
@@ -1,19 +1,20 @@
 function handleFormSubmit(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    const nameInput = document.querySelector('#name');
-    const emailInput = document.querySelector('#email');
-  
-    const nameValue = nameInput.value.trim();
-    const emailValue = emailInput.value.trim();
-  
-    if (nameValue === '' || emailValue === '') {
-      alert('Please fill in all required fields.');
-    } else {
-      alert('Form submitted successfully!');
-      document.querySelector('#submitForm').submit();
-    }
+  event.preventDefault(); // Prevent form submission
+
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const messageElement = document.getElementById('message');
+
+  const nameValue = nameInput.value.trim();
+  const emailValue = emailInput.value.trim();
+
+  if (nameValue === '' || emailValue === '') {
+    messageElement.innerHTML = 'Please fill in all required fields.';
+  } else {
+    messageElement.innerHTML = 'Form submitted successfully!';
+    document.getElementById('submitForm').submit();
   }
-  
-  // Add event listener to the form element
-  document.querySelector('#submitForm').addEventListener('submit', handleFormSubmit);
+}
+
+// Add event listener to the form element
+document.getElementById('submitForm').addEventListener('submit', handleFormSubmit);
